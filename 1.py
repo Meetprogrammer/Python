931ceae589e8bcb6b57cb9782a204c45404bc698
Lets Create New <97238407+Meetprogrammer@users.noreply.github.com>
2023-06-29 02:15:47 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+a = int(420) #Integer
+b = "Subscribe to letscreatenew" #String
+c = 420 #Integer
+d = str("Subscribe to letscreatenew") #String
+print(a)
+print(b)
+print(c)
+print(d)
+#Guido Van Rossum 1991
+print("Hello World")
